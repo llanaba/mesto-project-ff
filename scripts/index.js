8acaae52946704c8f0757275329ff03fd39897f9
Екатерина Яковлева <yallanaba@yandex.ru>
2024-09-23 19:48:01 +0300

    creates adding and deleting cards functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
+// Функция создания карточки
+function addCard(cardData, deleteCard) {
+  // Клонируем темплейт карточки, ищем в нем все элементы:
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('li').cloneNode(true);
+  const cardTitleElement = cardElement.querySelector('.card__title');
+  const cardImageElement = cardElement.querySelector('.card__image');
+  const cardDeleteButtonElement = cardElement.querySelector('.card__delete-button');
+  
+  // Заполняем элементы карточки данными:
+  cardTitleElement.textContent = cardData.name;
+  cardImageElement.src = cardData.link
+  
+  // Вешаем обработчик событий на кнопку удаления:
+  cardDeleteButtonElement.addEventListener('click', function () {
+    deleteCard(cardElement);
+  });
 
-// @todo: DOM узлы
+  // Возвращаем готовый элемент карточки:
+  return cardElement;
+}
 
-// @todo: Функция создания карточки
+// Функция удаления карточки:
+function deleteCard(card) {
+  card.remove();
+}
 
-// @todo: Функция удаления карточки
+// Выводим карточки на страницу:
+const cardListContainer = document.querySelector('.places__list');
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  const card = addCard(cardData, deleteCard);
+  cardListContainer.append(card);
+})
